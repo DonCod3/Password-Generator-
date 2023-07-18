5314544a1a6c53c8b4c27e74fbe3016634e73b2a
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-07-17 22:59:13 -0400

    fixed linked files & added to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var Uppercasechar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Lowercasechar = "abcdefghijklmnopqrstuvwxyz";
+var numberchar = "0123456789";
+var specialchars = "!@#$%^&*()_+{}[]\;':,<>";
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +16,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Psudo code
+///password criteria: must be at least 8 characters, no more than 128
+///character reqts: at least 1 special character, 1 upper case & 1 numeric; lowercase accepted
+///when entering a password, input should be accepted/validated or declined/rejected
+////display new passwoord once password is generated 
+
