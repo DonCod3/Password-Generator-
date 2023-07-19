d7dde3a63d0fd90c130f2e09d017c11b4757eec6
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-07-18 22:56:10 -0400

    password feedback

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var Uppercasechar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var Lowercasechar = "abcdefghijklmnopqrstuvwxyz";
-var numberchar = "0123456789";
-var specialchars = "!@#$%^&*()_+{}[]\;':,<>";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  //incorrect password feedback; Using If statement 
+  if (password == undefined) {
+    passwordText.value = "Password error Please try again."
+  } else if (password == 0) {
+    passwordText.value = "No password entered. Please try again."
+  } else {
   passwordText.value = password;
+  }
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 
 
 //Psudo code
@@ -24,3 +28,8 @@ generateBtn.addEventListener("click", writePassword);
 ///when entering a password, input should be accepted/validated or declined/rejected
 ////display new passwoord once password is generated 
 
+var Uppercasechar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Lowercasechar = "abcdefghijklmnopqrstuvwxyz";
+var numberchar = "0123456789";
+var specialchars = "!@#$%^&*()_+{}[]\;':,<>";
+var passwordLength = 
